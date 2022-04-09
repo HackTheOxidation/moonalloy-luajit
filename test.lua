faa235578267ac96d65f14825d49f4efb0932939
HackTheOxidation <tomas.hagenau@protonmail.ch>
2022-04-09 18:57:43 +0200

    Removed redundant size from test.lua

--- test.lua
+++ test.lua
@@ -4,7 +4,7 @@ local function test_Array()
   local arg = {1.0, 2.0, 3.0}
 
   local a = Array.new(arg)
-  print("a = ", a, ", ", a.len)
+  print("a = ", a)
 
   local b = Array.new({2.0, 3.0, 5.0})
   print("b = ", b)
